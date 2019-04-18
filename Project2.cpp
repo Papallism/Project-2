c5a7e38c53f2fd503c416245591f112c22d38eb2
Michalis Papallis <mihalispapallis@gmail.com>
2019-04-18 19:39:00 +0300

    Add files via upload
    added account struct

--- Project2.cpp
+++ Project2.cpp
@@ -2,11 +2,17 @@
 #include <cstring>
 using namespace std;
 
+struct Account {
+    int number;
+    char type[20];
+    float balance;
+};
+
 struct Customer {
     char name[30];
     char address[50];
     char id[10];
-
+    Account accounts[5];
     int totAccounts;
 };
 
